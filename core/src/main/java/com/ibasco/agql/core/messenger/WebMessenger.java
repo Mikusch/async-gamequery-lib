8e822ac4b4a2d8e35bebd84b9203e74bd4cc86a1
Rafael Ibasco <ribasco@gmail.com>
2019-03-29 05:13:22 +0800

    Issue #36: Allow clients to accept a custom ExecutorService

--- core/src/main/java/com/ibasco/agql/core/messenger/WebMessenger.java
+++ core/src/main/java/com/ibasco/agql/core/messenger/WebMessenger.java
@@ -30,6 +30,9 @@ import com.ibasco.agql.core.Messenger;
 import com.ibasco.agql.core.Transport;
 import com.ibasco.agql.core.transport.http.AsyncHttpTransport;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.asynchttpclient.Request;
 import org.asynchttpclient.Response;
@@ -39,27 +42,37 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.function.Function;
 
 /**
  * A Messenger responsible for handling Web Requests and Responses.
  */
 //TODO: Implement a generic request/response lookup service
-public class WebMessenger<Req extends AbstractWebRequest, Res extends AbstractWebResponse> implements Messenger<Req, Res> {
+public class WebMessenger<R extends AbstractWebRequest, S extends AbstractWebResponse> implements Messenger<R, S> {
 
     private static final Logger log = LoggerFactory.getLogger(WebMessenger.class);
-    private Transport<Request> transport;
-    private Function<Response, Res> responseFactory;
 
-    public WebMessenger(Function<Response, Res> responseFactory) {
+    private Transport<Request> transport;
+
+    private Function<Response, S> responseFactory;
+
+    public WebMessenger(Function<Response, S> responseFactory) {
         this(responseFactory, null);
     }
 
-    public WebMessenger(Function<Response, Res> responseFactory, EventLoopGroup eventLoopGroup) {
+    public WebMessenger(Function<Response, S> responseFactory, ExecutorService executorService) {
         DefaultAsyncHttpClientConfig.Builder configBuilder = new DefaultAsyncHttpClientConfig.Builder();
         configBuilder.setKeepAlive(true);
         configBuilder.addRequestFilter(new ThrottleRequestFilter(40));
-        if (eventLoopGroup != null) {
+
+        if (executorService != null) {
+            EventLoopGroup eventLoopGroup;
+            if (Epoll.isAvailable()) {
+                eventLoopGroup = new EpollEventLoopGroup(0, executorService);
+            } else {
+                eventLoopGroup = new NioEventLoopGroup(0, executorService);
+            }
             configBuilder.setEventLoopGroup(eventLoopGroup);
         }
         this.transport = new AsyncHttpTransport(configBuilder.build());
@@ -67,23 +80,23 @@ public class WebMessenger<Req extends AbstractWebRequest, Res extends AbstractWe
     }
 
     @Override
-    public CompletableFuture<Res> send(Req request) {
+    public CompletableFuture<S> send(R request) {
         log.debug("Sending request with url : {}", request.getMessage().getUri());
         CompletableFuture<Response> res = transport.send(request.getMessage());
         //transform the raw Response type to an instance of AbstractWebResponse using the supplied factory method
         return res.thenApply(responseFactory);
     }
 
-    public Function<Response, Res> getResponseFactory() {
+    public Function<Response, S> getResponseFactory() {
         return responseFactory;
     }
 
-    public void setResponseFactory(Function<Response, Res> responseFactory) {
+    public void setResponseFactory(Function<Response, S> responseFactory) {
         this.responseFactory = responseFactory;
     }
 
     @Override
-    public void accept(Res res, Throwable throwable) {
+    public void accept(S res, Throwable throwable) {
         //do nothing
     }
 
