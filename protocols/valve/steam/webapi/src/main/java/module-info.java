cec4cf94381aa25900e3f13331cc34e71f14d02e
Rafael Ibasco <ribasco@gmail.com>
2019-04-02 11:11:33 +0800

    Add opens to gson

--- protocols/valve/steam/webapi/src/main/java/module-info.java
+++ protocols/valve/steam/webapi/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module agql.steam.webapi {
     exports com.ibasco.agql.protocols.valve.steam.webapi.interfaces;
     exports com.ibasco.agql.protocols.valve.steam.webapi.pojos;
     exports com.ibasco.agql.protocols.valve.steam.webapi.enums;
+    opens com.ibasco.agql.protocols.valve.steam.webapi.pojos to gson;
 
     requires gson;
     requires org.apache.commons.lang3;
