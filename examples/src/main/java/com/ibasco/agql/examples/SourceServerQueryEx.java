36a37e8d6be397aff5160535a043154b8a8f0c08
Rafael Luis Ibasco <ribasco@gmail.com>
2021-09-19 13:02:54 +0800

    Added new option to test query for a single server instance
    Signed-off-by: Rafael Luis Ibasco <ribasco@gmail.com>

--- examples/src/main/java/com/ibasco/agql/examples/SourceServerQueryEx.java
+++ examples/src/main/java/com/ibasco/agql/examples/SourceServerQueryEx.java
@@ -26,23 +26,32 @@ import com.ibasco.agql.protocols.valve.source.query.pojos.SourceServer;
 import com.ibasco.agql.protocols.valve.steam.master.client.MasterServerQueryClient;
 import com.ibasco.agql.protocols.valve.steam.master.enums.MasterServerRegion;
 import com.ibasco.agql.protocols.valve.steam.master.enums.MasterServerType;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Pattern;
 
 public class SourceServerQueryEx extends BaseExample {
 
     private static final Logger log = LoggerFactory.getLogger(SourceServerQueryEx.class);
+
+    private static final Pattern PATTERN_IP = Pattern.compile("(?<ip>\\b(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\\b)|\\:(?<port>\\d*)");
+
     private SourceQueryClient sourceQueryClient;
+
     private MasterServerQueryClient masterServerQueryClient;
 
     public static void main(String[] args) throws Exception {
@@ -50,33 +59,54 @@ public class SourceServerQueryEx extends BaseExample {
         q.run();
     }
 
-    public void queryAllServers() {
-        int appId = Integer.valueOf(promptInput("List servers only from this app id (int)", false, null, "srcQryAppId"));
-        Boolean emptyServers = promptInputBool("List only empty servers? (y/n)", false, null, "srcQryEmptySvrs");
-        Boolean passwordProtected = promptInputBool("List only passwd protected servers? (y/n)", false, null, "srcQryPassProtect");
-        Boolean dedicatedServers = promptInputBool("List only dedicated servers (y/n)", false, "y", "srcQryDedicated");
+    public void performQuery() throws Exception {
+        var queryAllServers = promptInputBool("Query all available servers? (y/n)", true, "y");
 
-        ServerFilter filter = ServerFilter.create()
-                .dedicated(dedicatedServers)
-                .isPasswordProtected(passwordProtected)
-                .allServers()
-                .isEmpty(emptyServers);
+        double start = 0.0d, end = 0.0d;
 
-        if (appId > 0)
-            filter.appId(appId);
+        if (queryAllServers) {
+            int appId = Integer.parseInt(promptInput("List servers only from this app id (int)", false, null, "srcQryAppId"));
+            Boolean emptyServers = promptInputBool("List only empty servers? (y/n)", false, null, "srcQryEmptySvrs");
+            Boolean passwordProtected = promptInputBool("List only passwd protected servers? (y/n)", false, null, "srcQryPassProtect");
+            Boolean dedicatedServers = promptInputBool("List only dedicated servers (y/n)", false, "y", "srcQryDedicated");
+            var filter = ServerFilter.create()
+                                     .dedicated(dedicatedServers)
+                                     .isPasswordProtected(passwordProtected)
+                                     .allServers()
+                                     .isEmpty(emptyServers);
 
-        log.info("Using filter: {}", filter.toString());
+            if (appId > 0)
+                filter.appId(appId);
 
-        double start = System.currentTimeMillis();
-        queryAllServers(filter);
-        double end = ((System.currentTimeMillis() - start) / 1000) / 60;
+            log.info("Using filter: {}", filter.toString());
+
+            start = System.currentTimeMillis();
+            performQuery(filter);
+            end = ((System.currentTimeMillis() - start) / 1000) / 60;
+        } else {
+            String addressString = promptInput("Enter the address of the server you want to query (<ip>:<port>)", true, "127.0.0.1:27015");
+            InetSocketAddress address = parseAddress(addressString);
+
+            log.info("Querying server address: {}:{}", address.getHostString(), address.getPort());
+            start = System.currentTimeMillis();
+            log.info("Server Info: {}", performQuery(address));
+            end = ((System.currentTimeMillis() - start) / 1000) / 60;
+        }
+
         log.info("Test Completed  in {} minutes", end);
     }
 
-    private Map<String, Double> queryAllServers(ServerFilter filter) {
+    private SourceServer performQuery(InetSocketAddress address) throws ExecutionException, InterruptedException {
+        return sourceQueryClient.getServerInfo(address).whenComplete((sourceServer, serverInfoError) -> {
+            if (serverInfoError != null) {
+                log.error("[SERVER : ERROR] : {}", serverInfoError.getMessage());
+                return;
+            }
+            log.info("[SERVER : INFO] : {}", sourceServer);
+        }).get();
+    }
 
-        final Map<String, Double> resultMap = new HashMap<>();
-
+    private void performQuery(ServerFilter filter) {
         final AtomicInteger masterServerCtr = new AtomicInteger(), masterError = new AtomicInteger();
         final AtomicInteger serverInfoCtr = new AtomicInteger(), serverInfoErr = new AtomicInteger();
         final AtomicInteger challengeCtr = new AtomicInteger(), challengeErr = new AtomicInteger();
@@ -112,42 +142,42 @@ public class SourceServerQueryEx extends BaseExample {
 
                         //Get Challenge
                         CompletableFuture<Integer> challengeFuture = sourceQueryClient.getServerChallenge(SourceChallengeType.PLAYER, serverAddress)
-                                .whenComplete((challenge, serverChallengeError) -> {
-                                    if (serverChallengeError != null) {
-                                        log.error("[CHALLENGE : ERROR] Message: '{}')", serverChallengeError.getMessage());
-                                        challengeErr.incrementAndGet();
-                                        return;
-                                    }
-                                    log.info("[CHALLENGE : INFO] Challenge '{}'", challenge);
-                                    challengeCtr.incrementAndGet();
+                                                                                      .whenComplete((challenge, serverChallengeError) -> {
+                                                                                          if (serverChallengeError != null) {
+                                                                                              log.error("[CHALLENGE : ERROR] Message: '{}')", serverChallengeError.getMessage());
+                                                                                              challengeErr.incrementAndGet();
+                                                                                              return;
+                                                                                          }
+                                                                                          log.info("[CHALLENGE : INFO] Challenge '{}'", challenge);
+                                                                                          challengeCtr.incrementAndGet();
 
-                                    CompletableFuture<List<SourcePlayer>> playersFuture = sourceQueryClient.getPlayers(challenge, serverAddress);
+                                                                                          CompletableFuture<List<SourcePlayer>> playersFuture = sourceQueryClient.getPlayers(challenge, serverAddress);
 
-                                    playersFuture.whenComplete((players, playerError) -> {
-                                        if (playerError != null) {
-                                            log.error("[PLAYERS : ERROR] Message: '{}')", playerError.getMessage());
-                                            playersErr.incrementAndGet();
-                                            return;
-                                        }
-                                        playersCtr.incrementAndGet();
-                                        log.info("[PLAYERS : INFO] : Total Players for Server '{}' = {}", serverAddress, players.size());
-                                        players.forEach(player -> log.info("    Index: {}, Name: {}, Duration: {}, Score: {}", player.getIndex(), player.getName(), player.getDuration(), player.getScore()));
-                                    });
-                                    requestList.add(playersFuture);
+                                                                                          playersFuture.whenComplete((players, playerError) -> {
+                                                                                              if (playerError != null) {
+                                                                                                  log.error("[PLAYERS : ERROR] Message: '{}')", playerError.getMessage());
+                                                                                                  playersErr.incrementAndGet();
+                                                                                                  return;
+                                                                                              }
+                                                                                              playersCtr.incrementAndGet();
+                                                                                              log.info("[PLAYERS : INFO] : Total Players for Server '{}' = {}", serverAddress, players.size());
+                                                                                              players.forEach(player -> log.info("    Index: {}, Name: {}, Duration: {}, Score: {}", player.getIndex(), player.getName(), player.getDuration(), player.getScore()));
+                                                                                          });
+                                                                                          requestList.add(playersFuture);
 
-                                    CompletableFuture<Map<String, String>> rulesFuture = sourceQueryClient.getServerRules(challenge, serverAddress);
-                                    rulesFuture.whenComplete((rules, rulesError) -> {
-                                        if (rulesError != null) {
-                                            log.error("[RULES : ERROR] Message: '{}')", rulesError.getMessage());
-                                            rulesErr.incrementAndGet();
-                                            return;
-                                        }
-                                        rulesCtr.incrementAndGet();
-                                        log.info("[RULES : INFO] Total # of Rules for Server '{}' = {}", serverAddress, rules.size());
-                                        rules.forEach((key, value) -> log.info("    {} = {}", key, value));
-                                    });
-                                    requestList.add(rulesFuture);
-                                });
+                                                                                          CompletableFuture<Map<String, String>> rulesFuture = sourceQueryClient.getServerRules(challenge, serverAddress);
+                                                                                          rulesFuture.whenComplete((rules, rulesError) -> {
+                                                                                              if (rulesError != null) {
+                                                                                                  log.error("[RULES : ERROR] Message: '{}')", rulesError.getMessage());
+                                                                                                  rulesErr.incrementAndGet();
+                                                                                                  return;
+                                                                                              }
+                                                                                              rulesCtr.incrementAndGet();
+                                                                                              log.info("[RULES : INFO] Total # of Rules for Server '{}' = {}", serverAddress, rules.size());
+                                                                                              rules.forEach((key, value) -> log.info("    {} = {}", key, value));
+                                                                                          });
+                                                                                          requestList.add(rulesFuture);
+                                                                                      });
                         requestList.add(challengeFuture);
                     } catch (Exception e) {
                         log.error("Error occured inside the master list callback", e);
@@ -155,7 +185,7 @@ public class SourceServerQueryEx extends BaseExample {
                 }).get(); //masterServerList
 
                 log.info("Waiting for {} requests to complete", requestList.size());
-                CompletableFuture[] futures = requestList.toArray(new CompletableFuture[0]);
+                var futures = requestList.toArray(new CompletableFuture[0]);
                 CompletableFuture.allOf(futures).get();
             } catch (Exception e) {
                 log.error("Error occured during processing", e);
@@ -175,12 +205,29 @@ public class SourceServerQueryEx extends BaseExample {
                 log.info("   Total Rules Records Received: {}", rulesCtr);
                 log.info("   Total Rules Error: {}", rulesErr);
             }
-            return resultMap;
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return null;
+    }
+
+    private InetSocketAddress parseAddress(String address) throws ParseException {
+        if (address == null || address.isBlank())
+            throw new ParseException("Address cannot be blank", 0);
+        var matcher = PATTERN_IP.matcher(address);
+        if (!matcher.find())
+            throw new ParseException(String.format("Invalid address: '%s'", address), 0);
+        String ip = matcher.group("ip");
+        Integer port = null;
+        if (matcher.find()) {
+            String portStr = matcher.group("port");
+            if (StringUtils.isBlank(portStr) || !StringUtils.isNumeric(portStr))
+                throw new ParseException(String.format("Invalid port number '%s'", portStr), 0);
+            port = Integer.parseInt(portStr);
+        }
+        if (port == null)
+            port = 27015;
+        return new InetSocketAddress(ip, port);
     }
 
     @Override
@@ -192,9 +239,9 @@ public class SourceServerQueryEx extends BaseExample {
 
         log.info("NOTE: Depending on your selected criteria, the application may time some time to complete. You can review the log file(s) once the program exits.");
 
-        ConcurrentUtils.sleepUninterrupted(5000);
+        ConcurrentUtils.sleepUninterrupted(500);
 
-        this.queryAllServers();
+        this.performQuery();
     }
 
     @Override
